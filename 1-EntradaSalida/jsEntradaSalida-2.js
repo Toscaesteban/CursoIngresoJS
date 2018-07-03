bd3ec40ad5f12b39c6487cd59ee5640e32aff849
Toscaesteban <40776603+Toscaesteban@users.noreply.github.com>
2018-07-02 22:04:05 -0300

    Primera clase
    Primera clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,21 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+
+   var nombre;
+   //nombre="Esteban";
+   //alert (Esteban);
+
+   nombre = prompt("ingrese su nombre", "natalia natalia")
+   alert ("su nombre es:" +nombre);
+
+
+  //Comentaio de una linea//
+
+//var person = prompt("Please enter your name", "Harry Potter"); //
+
+
+  /* Informacion de W3schools */
 	
 }
 
