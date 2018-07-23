28b7b77be6e40cd86715ca910a18eb4054bbd9ff
Esteban Fernandez <40776603+Toscaesteban@users.noreply.github.com>
2018-07-23 10:05:05 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,28 @@
-function mostrar()
+function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mesDelAño){
 
+    case "Enero":
+    alert(mesDelAño +" que comiences bien el año!!!")
+    break
+
+    case "Marzo":
+	alert(mesDelAño + " a clases!!!")
+    break
+   
+    case "Julio":
+	alert(mesDelAño + " se vienen las vacaciones")
+    break
+
+    case"Diciembre":
+    alert(mesDelAño + " felices fiestas")
+    break
+}
+
+     
 
 
 }//FIN DE LA FUNCIÓN
